8777e0b725d5ad9c8c3710992d82df12b06ac7cf
WetU <yuk6196@gmail.com>
2015-12-31 02:50:47 +0900

    Create koKR locale lua file

---
+++ localization/localization.ko.lua
@@ -0,0 +1,22 @@
+if GetLocale() ~= 'koKR' then return end
+
+local AddonName, Addon = ...
+local L = Addon.L
+
+L = {
+ColorSettings = '색상',
+
+ColorSettingsTitle = 'tullaRange 색상 구성 설정',
+
+oor = '사정거리 벗어남',
+
+oom = '마나 부족',
+
+unusable = '사용불가',
+
+Red = '빨강',
+
+Green = '녹색',
+
+Blue = '파랑'
+}
